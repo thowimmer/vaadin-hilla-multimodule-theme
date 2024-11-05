bb5b08db09571ce75d2f9eeeb312a815dd75c616
thowimmer <thowimmer@proton.me>
2024-11-05 21:14:35 +0100

    Setup minimal project.

---
+++ app/src/main/java/com/thowimmer/example/Application.java
@@ -0,0 +1,15 @@
+package com.thowimmer.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Theme("my-theme")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ presentation/src/main/frontend/views/@index.tsx
@@ -0,0 +1,7 @@
+import {VerticalLayout} from "@vaadin/react-components";
+
+export default function TestView() {
+    return <VerticalLayout theme="padding spacing">
+        <h1>VAADIN MULTI-MODULE DEMO</h1>
+    </VerticalLayout>
+}
