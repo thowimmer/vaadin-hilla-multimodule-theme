0165263a372b8069dd10d0f43f5aad76927824fe
thowimmer <thowimmer@proton.me>
2024-11-07 09:18:52 +0100

    FIX: Add AppShellConfigurator in presentation module to ensure that CSS files are generated.

--- app/src/main/java/com/thowimmer/example/Application.java
+++ app/src/main/java/com/thowimmer/example/Application.java
@@ -1,13 +1,10 @@
 package com.thowimmer.example;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Theme("my-theme")
-public class Application implements AppShellConfigurator {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ presentation/src/main/java/com/thowimmer/example/FrontendConfigurator.java
@@ -0,0 +1,8 @@
+package com.thowimmer.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme("my-theme")
+class FrontendConfigurator implements AppShellConfigurator {
+}
